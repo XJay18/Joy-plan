afde91e1ca15f03a5d42842060e77302f8b6c72c
XJay18 <XJay18@users.noreply.github.com>
2019-03-18 13:47:02 +0800

    continue to work on fqz --jaycao

--- app/src/main/java/com/android/xjay/joyplan/PickerView.java
+++ app/src/main/java/com/android/xjay/joyplan/PickerView.java
@@ -55,7 +55,7 @@ public class PickerView extends View {
 
     // Interface for callback
     public interface OnSelectedListener {
-        void onChoose(View view, String selected);
+        void onSelect(View view, String selected);
     }
 
     private static class ScrollTimerTask extends TimerTask {
@@ -256,7 +256,7 @@ public class PickerView extends View {
                 cancelTimerTask();
 
                 if (mOnSelectListener != null && mSelectedIndex < mDataList.size()) {
-                    mOnSelectListener.onChoose(this, mDataList.get(mSelectedIndex));
+                    mOnSelectListener.onSelect(this, mDataList.get(mSelectedIndex));
                 }
             }
         } else if (mScrollDistance > 0) {
