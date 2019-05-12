e046a140936983fc95523eba75f91599c0ae5ee1
XJay18 <XJay18@users.noreply.github.com>
2019-05-12 12:17:02 +0800

    revise the layout of fqz --jaycao

--- app/src/main/java/com/android/xjay/joyplan/HelpnfeedbackSetupActivity.java
+++ app/src/main/java/com/android/xjay/joyplan/HelpnfeedbackSetupActivity.java
@@ -1,5 +1,6 @@
 package com.android.xjay.joyplan;
 
+import android.animation.Animator;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
@@ -7,6 +8,8 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import com.airbnb.lottie.LottieAnimationView;
 
 import org.w3c.dom.Text;
 
@@ -17,32 +20,59 @@ public class HelpnfeedbackSetupActivity extends AppCompatActivity implements Vie
     private EditText tv_mailaddress;
     private Button bt_back;
     private Button bt_confirm;
+    private LottieAnimationView animationView;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setup_helpnfeedback);
-        tv_feedback=(EditText)findViewById(R.id.et_setup_feedback);
-        tv_mailaddress=(EditText)findViewById(R.id.et_setup_mailaddress);
-        bt_back=(Button)findViewById(R.id.bt_setup_helpnfeedback_back);
-        bt_confirm=(Button)findViewById(R.id.btn_setup_confirm);
+        tv_feedback = (EditText) findViewById(R.id.et_setup_feedback);
+        tv_mailaddress = (EditText) findViewById(R.id.et_setup_mailaddress);
+        bt_back = (Button) findViewById(R.id.bt_setup_helpnfeedback_back);
+        bt_confirm = (Button) findViewById(R.id.btn_setup_confirm);
         setListener();
+        animationView = findViewById(R.id.setup_anim_okay_blue);
     }
 
-    private void setListener(){
+    private void setListener() {
         bt_back.setOnClickListener(this);
         bt_confirm.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
-        switch (v.getId()){
-            case R.id.bt_setup_helpnfeedback_back:{
+        switch (v.getId()) {
+            case R.id.bt_setup_helpnfeedback_back: {
                 finish();
                 break;
             }
-            case R.id.btn_setup_confirm:{
-                Toast.makeText(this,"您的反馈已经提交，谢谢您！",Toast.LENGTH_SHORT).show();
+            case R.id.btn_setup_confirm: {
+                animationView.setAnimation("okay_blue.json");
+                animationView.loop(false);
+                Animator.AnimatorListener mAnimationListener = new Animator.AnimatorListener() {
+                    @Override
+                    public void onAnimationStart(Animator animation) {
+                        animationView.setVisibility(View.VISIBLE);
+                    }
+
+                    @Override
+                    public void onAnimationEnd(Animator animation) {
+                        animationView.setVisibility(View.INVISIBLE);
+                    }
+
+                    @Override
+                    public void onAnimationCancel(Animator animation) {
+
+                    }
+
+                    @Override
+                    public void onAnimationRepeat(Animator animation) {
+
+                    }
+                };
+                animationView.addAnimatorListener(mAnimationListener);
+                animationView.playAnimation();
+                Toast.makeText(this, "您的反馈已经提交，谢谢您！", Toast.LENGTH_SHORT).show();
             }
             default:
                 break;
