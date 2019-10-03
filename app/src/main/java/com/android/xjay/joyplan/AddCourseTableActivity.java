6ea9d422c57a2aadf07687dd9ed3c68f98d113fd
kyleWu12138 <1466231545@qq.com>
2019-10-03 19:49:13 +0800

    able to import class schedule file

--- app/src/main/java/com/android/xjay/joyplan/AddCourseTableActivity.java
+++ app/src/main/java/com/android/xjay/joyplan/AddCourseTableActivity.java
@@ -2,12 +2,19 @@ package com.android.xjay.joyplan;
 
 import android.animation.Animator;
 import android.app.AlertDialog;
+import android.content.ContentUris;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.database.Cursor;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.DocumentsContract;
+import android.provider.MediaStore;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.text.Editable;
 import android.text.TextWatcher;
@@ -25,6 +32,8 @@ import com.airbnb.lottie.LottieAnimationView;
 import com.android.xjay.joyplan.Utils.JumpTextWatcher;
 import com.android.xjay.joyplan.Utils.POIExcelProcesser;
 
+import android.content.pm.*;
+import android.*;
 
 import org.apache.poi.ss.formula.functions.T;
 
@@ -134,6 +143,20 @@ public class AddCourseTableActivity extends AppCompatActivity implements View.On
             Intent intent=new Intent(Intent.ACTION_GET_CONTENT);
             intent.setType("application/vnd.openxmlformats-officedocument.spreadsheetml.sheet");
             intent.addCategory(Intent.CATEGORY_OPENABLE);
+            //检测是否有读取外储存的权限，动态获取
+            try {
+                //检测是否有写的权限
+                int permission = ActivityCompat.checkSelfPermission(this,
+                        "android.permission.WRITE_EXTERNAL_STORAGE");
+                if (permission != PackageManager.PERMISSION_GRANTED) {
+                    // 没有写的权限，去申请写的权限，会弹出对话框
+                    ActivityCompat.requestPermissions(this,
+                            new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 2);
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
             startActivityForResult(intent,1);
 
        }
@@ -142,24 +165,127 @@ public class AddCourseTableActivity extends AppCompatActivity implements View.On
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        POIExcelProcesser.setExceltoSchedule("/data/data/com.android.xjay.joyplan/files/123.xlsx",this);
-        Toast.makeText(this,"课程表导入成功",Toast.LENGTH_SHORT).show();
+
         if (requestCode == 1) {
-//            if (resultCode == RESULT_OK) {
-//                Uri uri = data.getData();
-//                if (uri != null) {
-//                    String path = getPath(this, uri);
-//                    if (path != null) {
-//                        File file = new File(path);
-//                        if (file.exists()) {
-//                            String upLoadFilePath = file.toString();
-//                            String upLoadFileName = file.getName();
-//                        }
-//                    }
-//                }
-//            }
+            if (resultCode == RESULT_OK) {
+                Uri uri = data.getData();
+                String path = getPath(this, uri).replaceAll("raw:","");
+//                POIExcelProcesser.setExceltoSchedule(/*"/storage/emulated/0/Download/123.xlsx"*/"/data/data/com.android.xjay.joyplan/files/123.xlsx", this);
+                POIExcelProcesser.setExceltoSchedule(path, this);
+                Toast.makeText(this, "课程表导入成功", Toast.LENGTH_SHORT).show();
+                Log.e("path",path);
+
+            }
+
         }
     }
+
+    public String getPath(final Context context, final Uri uri) {
+
+        final boolean isKitKat = Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT;
+
+        // DocumentProvider
+        if (isKitKat && DocumentsContract.isDocumentUri(context, uri)) {
+            // ExternalStorageProvider
+            if (isExternalStorageDocument(uri)) {
+                final String docId = DocumentsContract.getDocumentId(uri);
+//                Log.i(TAG,"isExternalStorageDocument***"+uri.toString());
+//                Log.i(TAG,"docId***"+docId);
+//                以下是打印示例：
+//                isExternalStorageDocument***content://com.android.externalstorage.documents/document/primary%3ATset%2FROC2018421103253.wav
+//                docId***primary:Test/ROC2018421103253.wav
+                final String[] split = docId.split(":");
+                final String type = split[0];
+
+                if ("primary".equalsIgnoreCase(type)) {
+                    return Environment.getExternalStorageDirectory() + "/" + split[1];
+                }
+            }
+            // DownloadsProvider
+            else if (isDownloadsDocument(uri)) {
+//                Log.i(TAG,"isDownloadsDocument***"+uri.toString());
+                final String id = DocumentsContract.getDocumentId(uri);
+                return id;
+//                final Uri contentUri = ContentUris.withAppendedId(
+//                Uri.parse("content://downloads/public_downloads"), Long.valueOf(id));
+//
+//                return getDataColumn(context, contentUri, null, null);
+            }
+            // MediaProvider
+            else if (isMediaDocument(uri)) {
+//                Log.i(TAG,"isMediaDocument***"+uri.toString());
+                final String docId = DocumentsContract.getDocumentId(uri);
+                final String[] split = docId.split(":");
+                final String type = split[0];
+
+                Uri contentUri = null;
+                if ("image".equals(type)) {
+                    contentUri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
+                } else if ("video".equals(type)) {
+                    contentUri = MediaStore.Video.Media.EXTERNAL_CONTENT_URI;
+                } else if ("audio".equals(type)) {
+                    contentUri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
+                }
+
+                final String selection = "_id=?";
+                final String[] selectionArgs = new String[]{split[1]};
+
+                return getDataColumn(context, contentUri, selection, selectionArgs);
+            }
+        }
+
+        else if ("file".equalsIgnoreCase(uri.getScheme())) {
+            return uri.getPath();
+        }
+        return null;
+    }
+
+    /**
+     * Get the value of the data column for this Uri. This is useful for
+     * MediaStore Uris, and other file-based ContentProviders.
+     *
+     * @param context       The context.
+     * @param uri           The Uri to query.
+     * @param selection     (Optional) Filter used in the query.
+     * @param selectionArgs (Optional) Selection arguments used in the query.
+     * @return The value of the _data column, which is typically a file path.
+     */
+    public String getDataColumn(Context context, Uri uri, String selection,
+                                String[] selectionArgs) {
+
+        Cursor cursor = null;
+        final String column = "_data";
+        final String[] projection = {column};
+
+        try {
+            cursor = context.getContentResolver().query(uri, projection, selection, selectionArgs,
+                    null);
+            if (cursor != null && cursor.moveToFirst()) {
+                final int column_index = cursor.getColumnIndexOrThrow(column);
+                return cursor.getString(column_index);
+            }
+        } finally {
+            if (cursor != null)
+                cursor.close();
+        }
+        return null;
+    }
+
+
+
+        public boolean isDownloadsDocument(Uri uri) {
+                return "com.android.providers.downloads.documents".equals(uri.getAuthority());
+            }
+
+        public boolean isMediaDocument(Uri uri) {
+                return "com.android.providers.media.documents".equals(uri.getAuthority());
+            }
+
+        public boolean isExternalStorageDocument(Uri uri) {
+        return ("com.android.externalstorage.documents".equals(uri.getAuthority()));
+    }
+
+
 
         private void sentBroadcast() {
         Intent intent = new Intent();
